0013ad08b5405ccd0642d32939c9e1a0d4532fb3
emmanuel mahngo <emmanuellmhango@gmail.com>
2025-08-07 13:54:01 +0200

    Add description and title of the data

---
+++ R/dowaodf.R
@@ -0,0 +1,42 @@
+#' ODF Sanitation and Hygiene Household Survey – Ntchisi & Dowa District 2019
+#'
+#' This dataset contains results from a household-level WASH (Water, Sanitation, and Hygiene) survey conducted on July 1st, 2019 in Mduku GVH, Dowa District, within the catchment area of Mbingwa Health Centre, Malawi. The purpose of the survey was to assess sanitation infrastructure, hygiene practices, and menstrual hygiene management (MHM) as part of an Open Defecation Free (ODF) verification exercise.
+#'
+#' @format A tibble with 939 rows and 34 variables
+#' \describe{
+#'   \item{survey_date}{Date the household survey was completed}
+#'   \item{district}{Name of the district where the household is located}
+#'   \item{gvh}{Name of the Group Village Headman (GVH) area}
+#'   \item{health_facility}{Name of the nearest or associated health facility}
+#'   \item{sanitation_observed}{Whether a sanitation facility was observed at the household}
+#'   \item{sanitation_type}{Type of sanitation facility observed (e.g., pit latrine, flush toilet)}
+#'   \item{pit_cover_or_seal}{Whether the facility has a pit cover or water seal to block flies}
+#'   \item{sanitation_near_water}{Whether the sanitation facility is located within 30m of a water point}
+#'   \item{sanitation_distance}{Approximate distance of the facility from the household}
+#'   \item{has_privacy}{Whether the sanitation facility offers visual privacy}
+#'   \item{has_security}{Whether the sanitation facility provides physical security}
+#'   \item{has_roof}{Whether the facility has a roof to prevent rain entry}
+#'   \item{shared_sanitation}{Whether the sanitation facility is shared with other households}
+#'   \item{child_faeces_disposal}{How faeces of children are disposed of (e.g., in toilet, open area)}
+#'   \item{child_faeces_disposal_other}{If other disposal method is used, specify it}
+#'   \item{usual_defecation_place}{Where household members usually defecate}
+#'   \item{faeces_seen_in_village}{Whether respondent observed faeces in the village in the past 6 months}
+#'   \item{faeces_around_house}{Whether faeces were observed around the household at the time of visit}
+#'   \item{handwash_near_sanitation}{Whether a handwashing facility is within 10 paces of the sanitation facility}
+#'   \item{handwash_has_water}{Whether the handwashing facility has water available}
+#'   \item{handwash_has_soap}{Whether soap is available at the handwashing station}
+#'   \item{handwash_in_use}{Whether there is evidence that the handwashing station is being used}
+#'   \item{mhm_materials_used}{Menstrual hygiene materials used by women in the household}
+#'   \item{mhm_cleaned}{Whether reusable menstrual materials are cleaned}
+#'   \item{mhm_cleaning_method}{How menstrual materials are cleaned (e.g., water and soap)}
+#'   \item{mhm_drying_method}{How menstrual materials are dried (e.g., sun, inside room)}
+#'   \item{mhm_disposal_place}{Where menstrual materials are disposed of}
+#'   \item{has_bathroom}{Whether the household has a designated bathroom facility}
+#'   \item{bathing_has_water}{Whether sufficient water is available for bathing}
+#'   \item{bathing_water_source}{Source of water used for bathing (e.g., borehole, unprotected spring)}
+#'   \item{bathroom_has_soap}{Whether soap is available at the bathroom facility}
+#'   \item{bathroom_in_use}{Whether there is evidence that the bathroom facility is being used}
+#'   \item{bathroom_has_privacy}{Whether the bathroom offers visual privacy}
+#'   \item{bathroom_has_security}{Whether the bathroom provides physical security}
+#' }
+"dowaodf"
